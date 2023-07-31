bd1b458ee4d2830173ff3ba7ba07a8666cb7d541
김도현 <minalov2@naver.com>
2023-07-31 12:07:28 +0900

    Update Ex_17478_재귀함수가뭔가요.java

--- 03-Algorithm/BOJ/17478-재귀함수가 뭔가요/Ex_17478_재귀함수가뭔가요.java
+++ 03-Algorithm/BOJ/17478-재귀함수가 뭔가요/Ex_17478_재귀함수가뭔가요.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Ex_17478_재귀함수가뭔가요 {
+public class Main_17478_김도현 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
