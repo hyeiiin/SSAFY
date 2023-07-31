acd73c6e5e13b272c7d49e64d1062e76af32a809
이도훈 <nestour95@gmail.com>
2023-07-31 16:51:18 +0900

    solve : Solution_1289_이도훈

--- 03-Algorithm/SWEA/1289-원재의 메모리 복구하기/Solution_1289_이도훈.java
+++ 03-Algorithm/SWEA/1289-원재의 메모리 복구하기/Solution_1289_이도훈.java
@@ -8,12 +8,12 @@ class Solution_1289_이도훈 {
         int T;
         T = sc.nextInt();
         sc.nextLine();
-
-
+        
         for (int test_case = 1; test_case <= T; test_case++) {
 
             String s = sc.nextLine();
 
+            // 비트를 boolean으로 처리하기 위한 배열
             boolean[] input = new boolean[s.length()];
             boolean[] comp = new boolean[s.length()];
 
@@ -23,10 +23,15 @@ class Solution_1289_이도훈 {
                 }
             }
 
+            // 몇번 딸깍했는지 체크하는 변수
             int changeCnt = 0;
+            
+            // 자리 돌면서
             for (int i = 0; i < input.length; i++) {
+                // 만약 딸깍을 짝수만큼 안했으면 기존에서 변화가 존재하는 것이므로 변화를 줌
                 if (changeCnt % 2 != 0) comp[i] = !comp[i];
 
+                // 값 비교
                 if (input[i] != comp[i]) {
                     changeCnt++;
                 }
