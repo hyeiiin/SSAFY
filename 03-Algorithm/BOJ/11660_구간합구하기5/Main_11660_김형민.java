45054b379fb94185ab1cdc64e6eb3aa57ecb6864
김형민 <rlagudals0420@naver.com>
2023-08-02 16:21:44 +0900

    Upload New File

---
+++ 03-Algorithm/BOJ/11660_구간합구하기5/Main_11660_김형민.java
@@ -0,0 +1,51 @@
+package boj;
+import java.io.*;
+import java.util.*;
+
+public class Main_11660_김형민 {
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());//표의 크기
+        int M = Integer.parseInt(st.nextToken());//합을 구해야하는 횟수 M
+        int[][] dp = new int[N + 1][N + 1];
+        int[][] arr = new int[N + 2][N + 2];
+        for (int y = 1; y < N+1; y++) {
+            st = new StringTokenizer(br.readLine());
+            for (int x = 1; x < N+1; x++) {
+                arr[y][x] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+
+
+        for (int y = 1; y < N+1; y++) {
+            for (int x = 1; x < N+1; x++) {
+                dp[y][x] = arr[y][x]
+                        + dp[y-1][x]
+                        + dp[y][x-1]
+                        - dp[y-1][x-1];
+            }
+        }
+
+
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            sb.append(dp[x2][y2]
+                    -dp[x2][y1-1]
+                    -dp[x1-1][y2]
+                    +dp[x1-1][y1-1]
+            ).append("\n");
+        }
+        System.out.println(sb);
+
+
+    }
+}
