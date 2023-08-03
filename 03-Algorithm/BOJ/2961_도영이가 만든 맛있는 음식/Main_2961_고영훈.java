f769f558c7413ed74a18006618a712119a0c10fc
고영훈 <koyo@koyo.kr>
2023-08-03 16:13:33 +0900

    Update 03-Algorithm/BOJ/2961_도영이가 만든 맛있는 음식/Main_2961_고영훈.java

--- 03-Algorithm/BOJ/2961_도영이가 만든 맛있는 음식/Main_2961_고영훈.java
+++ 03-Algorithm/BOJ/2961_도영이가 만든 맛있는 음식/Main_2961_고영훈.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main_2961_고영훈 {
 	static int N;
 	static int[] sours;
 	static int[] bitters;
-	
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		sours = new int[N];
@@ -21,22 +18,17 @@ public class Main_2961_고영훈 {
 			sours[i] = Integer.parseInt(st.nextToken());
 			bitters[i] = Integer.parseInt(st.nextToken());
 		}
-		final int diff = generateSubset(0, 0, 1, 0);
-		bw.write(Integer.toString(diff));
-		bw.write("\n");
-		bw.flush();
+		final int diff = generateSubset(0, false, 1, 0);
+		System.out.println(Integer.toString(diff));
 	}
-	
-	public static int generateSubset(int index, int count, int sour, int bitter) {
+
+	public static int generateSubset(int index, boolean food, int sour, int bitter) {
 		if (index == N) {
-			if (count == 0) {
-				return Integer.MAX_VALUE;
-			}
-			return Math.abs(sour - bitter);
+			return food ? Math.abs(sour - bitter) : Integer.MAX_VALUE;
 		}
 		return Math.min(
-				generateSubset(index + 1, count + 1, sour * sours[index], bitter + bitters[index]),
-				generateSubset(index + 1, count, sour, bitter)
+			generateSubset(index + 1, true, sour * sours[index], bitter + bitters[index]),
+			generateSubset(index + 1, food, sour, bitter)
 		);
 	}
 }
