f8cf72745eee3863643042273c61eac5ae09c76a
김아현 <dhflxhdxhd@gmail.com>
2023-08-04 16:00:29 +0900

    update : 메소드 분리

--- 03-Algorithm/SWEA/1218_괄호 짝짓기/Solution_1218_김아현.java
+++ 03-Algorithm/SWEA/1218_괄호 짝짓기/Solution_1218_김아현.java
@@ -1,43 +1,26 @@
-package ssafy.Swea;
-
 import java.util.*;
 import java.io.*;
 
-public class _1218_Swea {
-
+public class Solution_1218_김아현 {
 	static Stack<Character> stack;
-	static boolean check=true;
+	static boolean check = true;
 	static char[] numArr;
+
 	public static void main(String[] args) throws Exception {
 		int test = 10;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		for (int t = 1; t <= test; t++) {
 			Integer.parseInt(br.readLine());
 			stack = new Stack<>();
 			numArr = br.readLine().toCharArray();
-	
+			check = true;
+
 			for (char num : numArr) {
-				if (num == '{' || num == '[' || num == '(' || num == '<') {
-					stack.push(num);
-				} else {
-					if (stack.isEmpty()) {
-						check = false;
-						break;
-					} else {
-						if (num == '}' && stack.peek() == '{') {
-							stack.pop();
-						} else if (num == ']' && stack.peek() == '[') {
-							stack.pop();
-						} else if (num == ')' && stack.peek() == '(') {
-							stack.pop();
-						} else if (num == '>' && stack.peek() == '<') {
-							stack.pop();
-						} else {
-							check = false;
-							break;
-						}
-					}
+				check = checkBracket(num);
+				
+				if(check == false) {
+					break;
 				}
 			}
 
@@ -47,7 +30,29 @@ public class _1218_Swea {
 				System.out.println("#" + t + " " + 0);
 			}
 		}
-
 	}
 
+	public static boolean checkBracket(char num) {
+		if (num == '{' || num == '[' || num == '(' || num == '<') {
+			stack.push(num);
+		} else {
+			if (stack.isEmpty()) {
+				return false;
+			} else {
+				if (num == '}' && stack.peek() == '{') {
+					stack.pop();
+				} else if (num == ']' && stack.peek() == '[') {
+					stack.pop();
+				} else if (num == ')' && stack.peek() == '(') {
+					stack.pop();
+				} else if (num == '>' && stack.peek() == '<') {
+					stack.pop();
+				} else {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
 }
