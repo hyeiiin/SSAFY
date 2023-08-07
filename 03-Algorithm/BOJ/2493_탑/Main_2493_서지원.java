158ad96e6332857cdb641930309294a7c15c9c17
서지원 <sjs3326@naver.com>
2023-08-07 11:34:58 +0900

    Upload New File

---
+++ 03-Algorithm/BOJ/2493_탑/Main_2493_서지원.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main_2493_서지원 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		Stack<int[]> stack = new Stack<>();
+		stack.push(new int[] {100000001, 0});
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			int h = Integer.parseInt(st.nextToken());
+			while (!stack.isEmpty() && stack.peek()[0] <= h) {
+				stack.pop();
+			}
+			sb.append(stack.peek()[1]).append(" ");
+			stack.push(new int[] {h, i + 1});
+		}	
+		sb.append("\n");
+		System.out.println(sb.toString());
+		
+	}
+}
