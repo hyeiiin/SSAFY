27cd6d76162ac9ec0b25a04a076fa4142640b72e
최지웅 <jwchoi42@naver.com>
2023-08-07 14:50:50 +0900

    Replace Main_2493_최지웅.java

--- 03-Algorithm/BOJ/2493_탑/Main_2493_최지웅.java
+++ 03-Algorithm/BOJ/2493_탑/Main_2493_최지웅.java
@@ -11,7 +11,7 @@ public class Main_2493_최지웅 {
 
 	public static void main(String[] args) throws Exception {
 
-		System.setIn(new FileInputStream("input.txt"));
+//		System.setIn(new FileInputStream("input.txt"));
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
