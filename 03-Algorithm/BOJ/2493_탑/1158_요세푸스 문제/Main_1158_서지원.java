b0dc2ece1d31b488b58657799aa02e483b075fc1
서지원 <sjs3326@naver.com>
2023-08-07 12:01:15 +0900

    Upload New File

---
+++ 03-Algorithm/BOJ/2493_탑/1158_요세푸스 문제/Main_1158_서지원.java
@@ -0,0 +1,45 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main_1158_서지원 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] temp = br.readLine().split(" ");
+		int n = Integer.parseInt(temp[0]);
+		int k = Integer.parseInt(temp[1]);
+		
+		List<Integer> list = new ArrayList<>();
+		for (int i = 1; i <= n; i++) {
+			list.add(i);
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<");
+		int idx = k - 1;
+		for (int i = 0; i < n; i++) {
+			if (idx < list.size()) {
+				sb.append(list.get(idx));
+				list.remove(idx);
+			} else {
+				idx %= list.size();
+				sb.append(list.get(idx));
+				list.remove(idx);
+			}
+			idx += k - 1;
+			if (i < n - 1) {
+				sb.append(", ");
+			}
+		}
+		sb.append(">");
+		System.out.println(sb.toString());
+		
+	}
+
+}
