10cdc7d01db5532a1de8569dc8233c21b33f5d8e
최지웅 <jwchoi42@naver.com>
2023-08-08 14:45:38 +0900

    Replace Main_16926_최지웅.java

--- 03-Algorithm/BOJ/16926_배열 돌리기 1/Main_16926_최지웅.java
+++ 03-Algorithm/BOJ/16926_배열 돌리기 1/Main_16926_최지웅.java
@@ -47,7 +47,7 @@ public class Main_16926_최지웅 {
 
 	public static void main(String[] args) throws Exception {
 
-		System.setIn(new FileInputStream("input.txt"));
+//		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
