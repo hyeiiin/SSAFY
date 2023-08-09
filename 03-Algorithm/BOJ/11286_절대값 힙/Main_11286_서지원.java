3ed9ee1bfc8802a8327f03195752a38d6a9bfa56
서지원 <sjs3326@naver.com>
2023-08-09 13:55:31 +0900

    Upload New File

---
+++ 03-Algorithm/BOJ/11286_절대값 힙/Main_11286_서지원.java
@@ -0,0 +1,43 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main_11286_서지원 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				int abs1 = Math.abs(o1), abs2 = Math.abs(o2);
+				if (abs1 == abs2) {
+					return o1 - o2;
+				}
+				return abs1 - abs2;
+			}
+		});
+		
+		int n = Integer.parseInt(br.readLine());
+		for (int i = 0; i < n; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if (x == 0) {
+				if (pq.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(pq.poll()).append("\n");
+				}
+			} else {
+				pq.offer(x);
+			}
+		}
+		System.out.println(sb.toString());
+	}
+	
+}
