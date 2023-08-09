ca01e81f83957edd132e51b5fe5b0cc28897ec68
김도현 <minalov2@naver.com>
2023-08-09 15:34:42 +0900

    Upload New File

---
+++ 03-Algorithm/BOJ/11286_절대값 힙/Main_11286_김도현.java
@@ -0,0 +1,43 @@
+package baekjun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main_11286_김도현 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				int compare = Math.abs(o1) - Math.abs(o2);
+				if(compare>0) {
+					return 1;
+				} else if(compare==0) {
+					return o1-o2;
+				}else {
+					return -1;
+				}
+			}
+		});
+
+        for (int i = 0; i < N; i++) {
+            int command = Integer.parseInt(br.readLine());
+            if (command == 0) {
+                if (queue.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(command);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
