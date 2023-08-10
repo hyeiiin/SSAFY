4e053ea50973fbc43a54d37c3e53b9de986adec7
최지웅 <jwchoi42@naver.com>
2023-08-10 16:34:44 +0900

    Replace Solution_4012_최지웅.java

--- 03-Algorithm/SWEA/4012_요리사/Solution_4012_최지웅.java
+++ 03-Algorithm/SWEA/4012_요리사/Solution_4012_최지웅.java
@@ -54,7 +54,7 @@ public class Solution_4012_최지웅 {
 	}
 
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("input.txt"));
+//		System.setIn(new FileInputStream("input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
