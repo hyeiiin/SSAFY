864d654385aaf3986ccc7e1c725b8f4e87e32f26
김나연 <kknyapple@gmail.com>
2023-08-16 15:30:51 +0900

    Add new file

---
+++ 03-Algorithm/BOJ/3109_빵집/Main_3109_김나연.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_3109_김나연 {
+	
+	static int r,c, visited[][], res, pipe;
+	static char a[][];
+	static int dy[]= {-1,0,1}, dx[]= {1,1,1};
+	
+	public static void go(int y, int x) {
+		visited[y][x]=1;
+		pipe=res;
+
+		if(x==c-1) {
+			res++;
+		}else {
+			for(int i=0;i<3;i++) {
+				int ny=y+dy[i];
+				int nx=x+dx[i];
+
+				if(ny<0||nx<0||ny>=r||nx>=c) continue;
+				if(a[ny][nx]=='x')continue;
+				if(visited[ny][nx]!=0)continue;
+
+				if(pipe!=res) return;
+				go(ny, nx);
+			}
+		}
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		r=Integer.parseInt(st.nextToken());
+		c=Integer.parseInt(st.nextToken());
+		a=new char[r][c];
+		
+		
+		for (int i = 0; i < r; i++) {
+			st = new StringTokenizer(br.readLine());
+			String str=st.nextToken();
+			for (int j = 0; j < c; j++) {
+				a[i][j]=(char) (str.charAt(j));
+			}
+		}
+
+		visited=new int[r][c];
+		for (int i = 0; i < r; i++) {
+			go(i,0);
+		}
+		
+		sb.append(res);
+		System.out.println(res);
+	}
+
+}
