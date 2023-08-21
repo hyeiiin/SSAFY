25362e95ad6bcc919e0ef06b0cd57582477c4624
이도훈 <nestour95@gmail.com>
2023-08-21 14:25:04 +0900

    Update Main_1697_이도훈.java

--- 03-Algorithm/BOJ/1697_숨바꼭질/Main_1697_이도훈.java
+++ 03-Algorithm/BOJ/1697_숨바꼭질/Main_1697_이도훈.java
@@ -1,67 +1,33 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
-public class Main_1697_이도훈 {
+public class Main_1697_이도훈{
 
-    static int N;
-    static int K;
-    static int answer;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String args[]) throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] dp = new int[100001];
+		dp[N] = 0;
 
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
+		// dp로 왼쪽 계산, 왼쪽 이동은 -1 이동 뿐이 못함
+		for (int i = N - 1; i >= 0; i--) {
+			dp[i] = dp[i + 1] + 1;
+		}
 
+		// 오른쪽 계산
+		for (int i = N + 1; i <= Math.min(100000, K); i++) {
+			// 현재 값 기준 -1 값이나 오른쪽 값 비교 후 1만큼 더해서 dp 저장
+			// 오른쪽의 경우에 1을 더해준 이유는 어차피 4 -> (5 / 2) -> 2 | 3 -> (4 / 2) -> 2 로 동일한 인덱스를 가지게 되고, 여기서 2로 딱 떨어졌는지 안떨어졌는지만 계산해서 횟수를 추가해주면 된다.
+			dp[i] = Math.min(dp[i - 1] + 1, dp[(i + 1) / 2] + 1 + (i % 2));
+		}
 
-        int[] dp = new int[100001];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-
-
-        Queue<Node> queue = new ArrayDeque<>();
-
-        queue.add(new Node(N, 0));
-
-        answer = Math.abs(N - K);
-
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-
-            dp[cur.pos] = Math.min(dp[cur.pos], cur.cnt);
-
-            int teleport = cur.pos * 2;
-            int forward = cur.pos + 1;
-            int back = cur.pos - 1;
-
-            int newCnt = cur.cnt + 1;
-            if(teleport <= 100000 && dp[teleport] > newCnt){
-                queue.add(new Node(teleport , newCnt));
-            }
-            if(forward <= 100000 && dp[forward] > newCnt){
-                queue.add(new Node(forward, newCnt));
-            }
-            if(back >= 0 && dp[back] > newCnt){
-                queue.add(new Node(back, newCnt));
-            }
-        }
-
-        System.out.println(dp[K]);
-    }
-
-    static class Node {
-        int pos;
-        int cnt;
-
-        public Node(int pos, int cnt) {
-            this.pos = pos;
-            this.cnt = cnt;
-        }
-    }
-
-
+		System.out.println(dp[K]);
+	}
 }
-
