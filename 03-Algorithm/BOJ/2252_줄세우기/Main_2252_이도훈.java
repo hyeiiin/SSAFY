4e70a0d8bce7a9da0d2c9814377c1bb6bea5385a
이도훈 <nestour95@gmail.com>
2023-08-21 15:18:46 +0900

    Update Main_2252_이도훈.java

--- 03-Algorithm/BOJ/2252_줄세우기/Main_2252_이도훈.java
+++ 03-Algorithm/BOJ/2252_줄세우기/Main_2252_이도훈.java
@@ -24,28 +24,34 @@ public class Main {
 
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		indegree = new int[N + 1];
-		indegree[0] = -1;
 
+		// 진입 차수 계산할 배열
+		indegree = new int[N + 1];
+
+		// 인접 리스트
 		adjList = new ArrayList[N + 1];
+
+		// 인접 리스트 초기화
 		for (int i = 1; i < adjList.length; i++) {
 			adjList[i] = new ArrayList<>();
 		}
 
+		// 인접 리스트 작성
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
 
-			adjList[from].add(to);
-			indegree[to]++;
+			adjList[from].add(to); // 연결 정보 추가
+			indegree[to]++; // 진입 차수 추가
 		}
 
+		// BFS
 		Queue<Integer> queue = new ArrayDeque<>();
 		StringBuilder sb = new StringBuilder();
 
 		for (int i = 1; i <= N; i++) {
-			if (indegree[i] == 0) {
+			if (indegree[i] == 0) { // 진입 차수가 0인 부분부터 위상 정렬 시작
 				queue.add(i);
 				sb.append(i).append(" ");
 			}
@@ -55,9 +61,9 @@ public class Main {
 			Integer cur = queue.poll();
 
 			for (Integer adj : adjList[cur]) {
-				indegree[adj]--;
+				indegree[adj]--; // 인접 노드의 진입 차수 하나 차감
 
-				if (indegree[adj] == 0) {
+				if (indegree[adj] == 0) { // 진입 차수가 0이면 더이상 들어올 곳이 없으므로 정렬
 					queue.add(adj);
 					sb.append(adj).append(" ");
 				}
