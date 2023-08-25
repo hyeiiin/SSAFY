268a295daf325c0eb8e750731140ffcfcb6b8d96
고영훈 <koyo@koyo.kr>
2023-08-25 11:54:53 +0900

    Upload New File

---
+++ 03-Algorithm/BOJ/1753_최단경로/Main_1753_고영훈.py
@@ -0,0 +1,38 @@
+from heapq import heappop, heappush
+from sys import stdin, stdout
+
+
+def main():
+    def dijkstra(start):
+        dists[start] = 0
+        pq = [(0, start)]
+        while pq:
+            d, u = heappop(pq)
+            if d > dists[u]:
+                continue
+            for v, w in adj_list[u]:
+                nd = d + w
+                if nd < dists[v]:
+                    dists[v] = nd
+                    heappush(pq, (nd, v))
+
+    V = int(stdin.buffer.readline().split()[0])
+    K = int(stdin.buffer.readline())
+    adj_list = [[] for _ in range(V + 1)]
+    for line in stdin.buffer.read().splitlines():
+        u, v, w = map(int, line.split())
+        adj_list[u].append((v, w))
+
+    INF = 1_000_000
+    dists = [INF] * (V + 1)
+    dijkstra(K)
+
+    write = stdout.write
+    for d in dists[1:]:
+        if d != INF:
+            write('%d\n' % d)
+        else:
+            write('INF\n')
+
+
+main()
