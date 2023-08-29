6a7f356c44d1867f1f9db832f9e1239eb20e89cc
서지원 <sjs3326@naver.com>
2023-08-29 14:04:25 +0900

    Upload New File

---
+++ 03-Algorithm/BOJ/1463_1로 만들기/Main_1463_서지원.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_1463_서지원 {
+
+	static int N;
+	static int[] dp;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		
+		dp = new int[1000001];
+		dp[1] = 0; dp[2] = 1; dp[3] = 1;
+		for (int i = 4; i <= N; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i / 3] + 1, dp[i]); 
+			} 
+			if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+			}
+		}
+		System.out.println(dp[N]);
+	}
+
+}
