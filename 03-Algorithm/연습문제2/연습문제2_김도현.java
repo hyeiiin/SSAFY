61164d19caac6ba06f8649da7978f541282b9cd2
김도현 <minalov2@naver.com>
2023-08-29 14:46:02 +0900

    Upload New File

---
+++ 03-Algorithm/연습문제2/연습문제2_김도현.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class 연습문제2_김도현 {
+
+	static int[] fn;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		fn(sc.nextInt());
+	}
+	public static void fn(int num) {
+		fn = new int[num+1];
+		fn[1] = 2;
+		fn[2] = 5;
+		for (int i = 3; i <= num; i++) {
+			fn[i] = fn[i-1]*2 + fn[i-2];
+		}
+		System.out.println(fn[num]);
+	}
+}
