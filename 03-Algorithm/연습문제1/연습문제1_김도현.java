69b22d94d57a23337fbd08a37a970ceb9e056e40
김도현 <minalov2@naver.com>
2023-08-29 14:00:05 +0900

    Update 연습문제1_김도현.java

--- 03-Algorithm/연습문제1/연습문제1_김도현.java
+++ 03-Algorithm/연습문제1/연습문제1_김도현.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class 연습문제1_김도현 {
 
+	static int fn[];
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] fn = new int[1000];
+		fn(sc.nextInt());
+	}
+	public static void fn(int num) {
+		fn = new int[num+1];
 		fn[1] = 2;
 		fn[2] = 3;
-		for (int i = 3; i < 1000; i++) {
+		for (int i = 3; i <= num; i++) {
 			fn[i] = fn[i-1]+fn[i-2];
 		}
-		for (int i = 3; i < fn.length; i++) {
-			System.out.print(fn[i]+" ");
-		}
+		System.out.println(fn[num]);
 	}
 
 }
