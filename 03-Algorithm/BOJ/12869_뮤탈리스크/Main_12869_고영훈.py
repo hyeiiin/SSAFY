0b36170393d7c36ee33a45d85abfb5327da8d07c
고영훈 <koyo@koyo.kr>
2023-08-29 15:06:09 +0900

    Update 03-Algorithm/BOJ/12869_뮤탈리스크/Main_12869_고영훈.py

--- 03-Algorithm/BOJ/12869_뮤탈리스크/Main_12869_고영훈.py
+++ 03-Algorithm/BOJ/12869_뮤탈리스크/Main_12869_고영훈.py
@@ -6,16 +6,14 @@ def main():
         hps.sort()
         i = sum((60 ** n) * hp for n, hp in enumerate(hps))
         if dp[i] == -1:
-            if not hps:
-                dp[i] = 0
-            else:
-                dp[i] = 1 + min(f([hp - d for hp, d in zip(hps, ds) if hp > d])
-                                for ds in permutations(damages[:len(hps)]))
+            dp[i] = 1 + min(f([hp - d for hp, d in zip(hps, ds) if hp > d])
+                            for ds in permutations(damages[:len(hps)]))
         return dp[i]
 
     damages = (9, 3, 1)
     N = int(input())
     dp = [-1] * sum(60 ** n for n in range(N + 1))
+    dp[0] = 0
     print(f(list(map(int, input().split()))))
 
 
