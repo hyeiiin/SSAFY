e57b02eb3a03772aa2909c696e4168560cc2ab92
서지원 <sjs3326@naver.com>
2023-08-29 13:51:15 +0900

    Replace 연습문제1_서지원.java

--- 03-Algorithm/연습문제1/연습문제1_서지원.java
+++ 03-Algorithm/연습문제1/연습문제1_서지원.java
@@ -10,15 +10,17 @@ public class 연습문제1_서지원 {
 	static int[] dp;
 	
 	private static int dp(int n) {
-		if (n <= 2) dp[n] = n + 1;
-		else dp[n] = dp(n - 1) + dp(n - 2);
+		dp = new int[N + 1];
+		dp[1] = 2; dp[2] = 3;
+		for (int i = 3; i <= n; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
 		return dp[n];
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		dp = new int[N + 1];
 		System.out.println(dp(N));
 	}
 
