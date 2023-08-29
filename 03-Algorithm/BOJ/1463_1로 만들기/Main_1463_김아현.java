566e4289bf9a59e5552a1e8128bf120edac2c491
김아현 <dhflxhdxhd@gmail.com>
2023-08-29 15:31:14 +0900

    Add : Main_1463_김아현.java

---
+++ 03-Algorithm/BOJ/1463_1로 만들기/Main_1463_김아현.java
@@ -0,0 +1,26 @@
+package ssafy.boj;
+
+import java.util.*;
+
+public class Main_1463_김아현 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int[] dp = new int[n+1];
+		
+		dp[0] = 0;
+		dp[1] = 0;
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i-1] + 1;
+			if(i%2 == 0) {
+				dp[i] = Math.min(dp[i/2]+1 , dp[i]);
+			}
+			
+			if(i%3 == 0) {
+				dp[i] = Math.min(dp[i/3]+1, dp[i]);
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}
