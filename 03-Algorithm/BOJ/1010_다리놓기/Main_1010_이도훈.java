d67c989a505607013832f21561518e35d6284841
이도훈 <nestour95@gmail.com>
2023-08-30 12:07:29 +0900

    Add new file

---
+++ 03-Algorithm/BOJ/1010_다리놓기/Main_1010_이도훈.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int T = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+
+		StringBuilder sb = new StringBuilder();
+
+		long[][] dp = new long[30][30];
+
+		for (int i = 0; i < 30; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || i == j) {
+					dp[i][j] = 1L;
+				} else {
+					dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+				}
+			}
+		}
+
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+
+			if (N < M) {
+				int temp = N;
+				N = M;
+				M = temp;
+			}
+
+			sb.append(dp[N][M])
+				.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+
+}
