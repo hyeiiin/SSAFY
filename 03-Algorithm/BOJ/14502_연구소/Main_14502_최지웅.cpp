13ad61454e600931a9e184597b835c7d69afbb4d
최지웅 <jwchoi42@naver.com>
2023-09-25 16:05:19 +0900

    Replace Main_14502_최지웅.cpp

--- 03-Algorithm/BOJ/14502_연구소/Main_14502_최지웅.cpp
+++ 03-Algorithm/BOJ/14502_연구소/Main_14502_최지웅.cpp
@@ -1,113 +1,78 @@
 #include <bits/stdc++.h>
 using namespace std;
-enum { EMPTY, WALL, VIRUS };
 void fastIO() {
-	ios::sync_with_stdio(false);
+	ios::sync_with_stdio;
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 }
+int n, m;
 const int MAXN = 8;
 const int MAXM = 8;
-int N, M;
 int arr[MAXN][MAXM];
-int arrCopy[MAXN][MAXM];
+enum { EMPTY, WALL, VIRUS };
 struct Pos {
-	int y, x;
-	Pos(int y, int x) {
-		this->y = y;
-		this->x = x;
-	}
-	string toString() {
-		return to_string(y) + " " + to_string(x);
+	int r, c;
+	Pos(int r, int c) {
+		this->r = r;
+		this->c = c;
 	}
 };
-vector<Pos> virusList;
-void copyArr() {
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < M; x++) {
-			arrCopy[y][x] = arr[y][x];
-		}
-	}
-}
-vector<Pos> wallList;
-void setWall() {
-	for (Pos pos : wallList) {
-		arrCopy[pos.y][pos.x] = WALL;
-	}
-}
+vector<Pos> emptyList;
+bool visited[MAXN][MAXM];
 const int dy[4] = { -1, 1, 0, 0 };
 const int dx[4] = { 0, 0, -1, 1 };
-bool valid(int y, int x) {
-	return 0 <= y && y < N && 0 <= x && x < M;
+bool valid(int r, int c) {
+	return 0 <= r && r < n && 0 <= c && c < m;
 }
-bool visited[MAXN][MAXM];
-void initVisited() {
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < M; x++) {
-			visited[y][x] = false;
-		}
+void spread(int y, int x) {
+	visited[y][x] = true;
+	for (int i = 0; i < 4; i++) {
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (valid(ny, nx) && arr[ny][nx] == EMPTY && !visited[ny][nx]) spread(ny, nx);
 	}
 }
-void spread(Pos pos) {
-	queue<Pos> q;
-	visited[pos.y][pos.x] = true;
-	q.push(pos);
-	while (!q.empty()) {
-		Pos pos = q.front();
-		q.pop();
-		//cout << pos.y << " " << pos.x << endl;
-		arrCopy[pos.y][pos.x] = VIRUS;
-		for (int i = 0; i < 4; i++) {
-			int ny = pos.y + dy[i];
-			int nx = pos.x + dx[i];
-			if (valid(ny, nx) && arrCopy[ny][nx] == EMPTY && !visited[ny][nx]) {
-				visited[ny][nx] = true;
-				q.push(Pos(ny, nx));
-			}
+int simul() {
+	memset(visited, false, sizeof(visited));
+	for (int r = 0; r < n; r++) {
+		for (int c = 0; c < m; c++) {
+			if (arr[r][c] == VIRUS) spread(r, c);
 		}
 	}
-}
-int countSafeArea() {
-	int area = 0;
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < M; x++) {
-			if (arrCopy[y][x] == EMPTY) area++;
+	int count = 0;
+	for (int r = 0; r < n; r++) {
+		for (int c = 0; c < m; c++) {
+			if (arr[r][c] == EMPTY && !visited[r][c]) count++;
 		}
 	}
-	return area;
+	return count;
 }
 int main() {
 	freopen("input.txt", "r", stdin);
 	fastIO();
-	cin >> N >> M;
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < M; x++) {
-			cin >> arr[y][x];
-			if (arr[y][x] == VIRUS) {
-				virusList.push_back(Pos(y, x));
+	cin >> n >> m;
+	for (int r = 0; r < n; r++) {
+		for (int c = 0; c < m; c++) {
+			cin >> arr[r][c];
+			if (arr[r][c] == EMPTY) {
+				emptyList.push_back(Pos(r, c));
 			}
 		}
 	}
-	int maxSafeArea = 0;
-	for (int i = 0; i < N * M; i++) {
-		for (int j = i + 1; j < N * M; j++) {
-			for (int k = j + 1; k < N * M; k++) {
-				if (arr[i / M][i % M] == EMPTY && arr[j / M][j % M] == EMPTY && arr[k / M][k % M] == EMPTY) {
-					copyArr();
-					wallList.push_back(Pos(i / M, i % M));
-					wallList.push_back(Pos(j / M, j % M));
-					wallList.push_back(Pos(k / M, k % M));
-					setWall();
-					initVisited();
-					for (Pos pos : virusList) {
-						if (!visited[pos.y][pos.x]) spread(pos);
-					}
-					maxSafeArea = max(maxSafeArea, countSafeArea());
-					wallList.clear();
-				}
+	int ans = 0;
+	for (int i = 0; i < emptyList.size(); i++) {
+		for (int j = 0; j < i; j++) {
+			for (int k = 0; k < j; k++) {
+				arr[emptyList[i].r][emptyList[i].c] = WALL;
+				arr[emptyList[j].r][emptyList[j].c] = WALL;
+				arr[emptyList[k].r][emptyList[k].c] = WALL;
+				ans = max(ans, simul());
+				arr[emptyList[i].r][emptyList[i].c] = EMPTY;
+				arr[emptyList[j].r][emptyList[j].c] = EMPTY;
+				arr[emptyList[k].r][emptyList[k].c] = EMPTY;
 			}
 		}
 	}
-	cout << maxSafeArea << endl;
+	cout << ans << endl;
 	return 0;
 }
