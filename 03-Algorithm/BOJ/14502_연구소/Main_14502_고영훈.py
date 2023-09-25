0a933354d125b44b4feb9916f11b8303b290bee1
고영훈 <koyo@koyo.kr>
2023-09-25 12:25:30 +0900

    Update Main_14502_고영훈.py

--- 03-Algorithm/BOJ/14502_연구소/Main_14502_고영훈.py
+++ 03-Algorithm/BOJ/14502_연구소/Main_14502_고영훈.py
@@ -1,20 +1,40 @@
-from itertools import combnations
+from collections import deque
+from itertools import combinations
 from sys import stdin
 
 
 def main():
-    input = stdin.readline
-    N = input()
-    T = N // 2
-    mat = tuple(tuple(map(int, input().split())) for _ in range(N))
-    pool = set(range(N))
-    answer = 100_000
-    for a_team in combnations(pool, T):
-        b_team = tuple(pool - set(a_team))
-        a = sum(mat[i][j] + mat[j][i] for i, j in combnations(a_team, 2))
-        b = sum(mat[i][j] + mat[j][i] for i, j in combnations(b_team, 2))
-        answer = max(answer, abs(a - b))
-    print(answer)
+    def bfs(q, visited, walls) -> int:
+        for y, x in walls:
+            visited |= (1 << (y * 8 + x))
+        q = deque(q)
+        count = 3
+        while q:
+            y, x = q.popleft()
+            for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                ny, nx = y + dy, x + dx
+                if 0 <= ny < N and 0 <= nx < M and not visited & (1 << (ny * 8 + nx)):
+                    visited |= (1 << (ny * 8 + nx))
+                    q.append((ny, nx))
+                    count += 1
+        return count
+
+    N, M = map(int, stdin.readline().split())
+    mat = [list(map(int, x[::2])) for x in stdin.read().splitlines()]
+    blanks = []
+    viruses = []
+    visited = 0
+    for y in range(N):
+        for x in range(M):
+            v = mat[y][x]
+            if mat[y][x] == 0:
+                blanks.append((y, x))
+            else:
+                if mat[y][x] == 2:
+                    viruses.append((y, x))
+                visited |= (1 << (y * 8 + x))
+    max_count = min(bfs(viruses, visited, walls) for walls in combinations(blanks, 3))
+    print(len(blanks) - max_count)
 
 
 main()
