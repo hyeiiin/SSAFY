6f899407ef3ff90c79aeb4d2df707233e737f43d
김도휘 <ehgnl97722@ajou.ac.kr>
2023-09-26 12:04:48 +0900

    Update 03-Algorithm/SWEA/3307_최장 증가 부분 수열/Solution_3307_김도휘.java

--- 03-Algorithm/SWEA/3307_최장 증가 부분 수열/Solution_3307_김도휘.java
+++ 03-Algorithm/SWEA/3307_최장 증가 부분 수열/Solution_3307_김도휘.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Solution {
     static int[] arr;
     static int[] ans;
 
