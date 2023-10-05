ec7b172a446980bab0bb6d08d9ab6388bca31988
고영훈 <koyo@koyo.kr>
2023-10-05 12:14:40 +0900

    Update Main_4485_고영훈.py

--- 03-Algorithm/BOJ/4485_녹색 옷 입은 애가 젤다지/Main_4485_고영훈.py
+++ 03-Algorithm/BOJ/4485_녹색 옷 입은 애가 젤다지/Main_4485_고영훈.py
@@ -1,4 +1,4 @@
-from collections import deque
+from heapq import heappop, heappush
 from sys import stdin
 
 
@@ -6,23 +6,27 @@ def main():
     input = stdin.readline
     INF = 156250
 
-    def bfs():
-        q = deque([(0, 0, mat[0][0])])
+    def dijkstra():
+        q = [(mat[0][0], 0, 0)]
         loss_mat = [[INF] * N for _ in range(N)]
         while q:
-            y, x, loss = q.popleft()
+            loss, y, x = heappop(q)
+            if loss > loss_mat[y][x]:
+                continue
             for dy, dx in ((-1, 0), (1, 0), (0, -1), (0, 1)):
                 ny, nx = y + dy, x + dx
-                if 0 <= ny < N and 0 <= nx < N and loss < loss_mat[ny][nx]:
-                    loss_mat[ny][nx] = loss
-                    q.append((ny, nx, loss + mat[ny][nx]))
-        return loss_mat[N - 1][N - 1] + mat[N - 1][N - 1]
+                if 0 <= ny < N and 0 <= nx < N:
+                    new_loss = loss + mat[ny][nx]
+                    if new_loss < loss_mat[ny][nx]:
+                        loss_mat[ny][nx] = new_loss
+                        heappush(q, (new_loss, ny, nx))
+        return loss_mat[N - 1][N - 1]
 
     problem = 0
     while N := int(input()):
         problem += 1
         mat = [list(map(int, input()[::2])) for _ in range(N)]
-        print(f'Problem {problem}: {bfs()}')
+        print(f'Problem {problem}: {dijkstra()}')
 
 
 main()
