7f3c5532e5082fcc8a9f058811aae4455ad4b4c6
김민석 <kms278711@naver.com>
2023-10-12 13:29:02 +0900

    Update Main_17472_김민석.java

--- 03-Algorithm/BOJ/17472_다리 만들기2/Main_17472_김민석.java
+++ 03-Algorithm/BOJ/17472_다리 만들기2/Main_17472_김민석.java
@@ -4,149 +4,138 @@ import java.util.*;
 import java.io.*;
 
 public class Main_17472_김민석 {
-	//가로, 세로, 지역넘버, 정답
-	private static int N,M, areaNum=1, answer;
-	//초기 배열
-	private static int[][] map;
-	//사방탐색
-	private static int[] dx = {-1,0,1,0};
-	private static int[] dy = {0,1,0,-1};
-	//지역넘버 붙일 때 사용할 방문배열
-	private static boolean[][] visited;
-	
-	//좌표 클래스
-	static class Point {
-		int x, y;
-		Point(int x, int y) {
-			this.x = x;
-			this.y = y;
+	//경로 클래스
+	//시작섬, 끝섬, 다리 길이
+	static class Path {
+		int start, end, len;
+		public Path(int start, int end, int len) {
+			super();
+			this.start = start;
+			this.end = end;
+			this.len = len;
+		}
+
+		@Override
+		public String toString() {
+			return "path [start=" + start + ", end=" + end + ", len=" + len + "]";
 		}
 	}
 	
-	//내부인지 체크
+	//가로,세로,섬번호
+	static int N,M,areaNum;
+	//사방탐색
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
+	//root배열
+	static int[] parents; 
+	//맵
+	static int[][] map;
+	//다리길이 오름차순으로 정렬을 위해 pq사용
+	static Queue<Path> pq = new PriorityQueue<>((o1,o2)-> o1.len-o2.len);
+	
+	//map을 벗어나지 않는 지 체크
 	private static boolean isIn(int x, int y) {
-		if(x>=0 && y>=0 && x<N && y<M) return true;
-		return false;
+		return x>=0 && y>=0 && x< N && y< M;
 	}
 	
-	//BFS 돌려서 구역 정하기
+	//섬 번호 세팅
 	private static void setArea(int x, int y) {
-		Queue<Point> queue = new ArrayDeque<>();
-		queue.offer(new Point(x,y));
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {x,y});
+		//탐색시작 좌표 번호 붙이기
+		map[x][y] = areaNum;
 		while(!queue.isEmpty()) {
-			Point cur = queue.poll();
-			map[cur.x][cur.y] = areaNum;
+			int[] cur = queue.poll();
+			//4방 탐색
 			for (int i = 0; i < 4; i++) {
-				int nx = cur.x+dx[i];
-				int ny = cur.y+dy[i];
-				if(isIn(nx,ny) && !visited[nx][ny] && map[nx][ny] == 1) {
-					visited[nx][ny] = true;
-					queue.offer(new Point(nx,ny));
+				int nx = cur[0] + dx[i];
+				int ny = cur[1] + dy[i];
+				//내부면서 섬이라면
+				if(isIn(nx,ny) && map[nx][ny] == -2) {
+					//섬 번호 부여
+					map[nx][ny] = areaNum;
+					//다음 탐색을 위해 큐에 추가
+					queue.offer(new int[] {nx,ny});
 				}
 			}
 		}
-		//다음 구역 번호를 위해 증가
+		//섬 하나가 번호 부여 끝나면 번호증가
 		areaNum++;
 	}
 	
-	//root가 같지 않으면 통일
-	private static boolean union(int a, int b, int[] parents) {
-		int aRoot = find(a, parents);
-		int bRoot = find(b, parents);
-		if(aRoot==bRoot) return false;
-		parents[aRoot] = bRoot;
-		return true;
-	}
-	
-	//root 찾기
-	private static int find(int a, int[] parents) {
-		if(parents[a] == a) return a;
-		return parents[a] = find(parents[a], parents);
-	}
-
-	//다리 붙여서 모든 경우의 수 찾기
-	private static void DFS(int[][] cur, int[] parents) {
-		//부모가 모두 같은지 체크할 변수
-		int check = 0;
-		for (int i = 1; i < parents.length; i++) {
-			if(parents[i] == i) {
-				check = i;
-				break;
-			}
-		}
-		//같은 개수 카운팅
-		int cnt = 0;
-		for (int i = 1; i < parents.length; i++) {
-			if(find(i,parents) == check) cnt ++;
-		}
-		
-		//부모가 모두 같다면
-		if(cnt == parents.length-1) {
-			//길 개수 카운팅
-			int answerTmp = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if(cur[i][j] < 0) {
-						answerTmp += Math.abs(cur[i][j]);
-					}
-				}
-			}
-			//정답 갱신
-			answer = Math.min(answer, answerTmp);
-			return;
-		}
-
+	//경로 세팅
+	private static void setRoutes() {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				//현재 좌표가 0보다 크면 
-				if(cur[i][j] > 0) {
-					//원본 cur 배열 복사를 위한 임시 배열
-					int curTmp[][] = new int[N][M];
-					//4방탐색
-					for (int k = 0; k < 4; k++) {
+				//섬이라면
+				if(map[i][j] != 0) {
+					//우, 하만 이용
+					for (int k = 1; k < 3; k++) {
 						int nx = i + dx[k];
 						int ny = j + dy[k];
-						//2칸여유가 있다면
-						if(isIn(nx,ny) && isIn(nx+dx[k], ny+dy[k])&& cur[nx][ny]<=0 && cur[nx+dx[k]][ny+dy[k]] <= 0) {
-							//원본 parent배열 복사
-							int[] parentsTmp = Arrays.copyOf(parents, parents.length);;
-							//원본 cur배열 복사
-							for (int l = 0; l < N; l++) {
-								curTmp[l] = Arrays.copyOf(cur[l], M);
-							}
-							//이동한 칸 -1하여 다리 놓기
-							curTmp[nx][ny] -= 1;
-							
-							//다리 놓을 수 있는 지 확인
+						//다리길이
+						int len = 1;
+						//다음 좌표가 내부면서 바다라면
+						if(isIn(nx,ny) && map[nx][ny] == 0) {
 							while(true) {
+								//다리 놓기
+								len ++;
 								nx += dx[k];
 								ny += dy[k];
-								//내부면서
-								if(isIn(nx,ny)) {
-									//다리를 놓을 수 있는 공간이면
-									if(curTmp[nx][ny] <= 0) {
-										//다리 놓기
-										curTmp[nx][ny] -= 1;
-									//내부면서 그렇지 않다면(어떤 섬에 도착했다면)
-									} else {
-										//해당 섬이 연결되어있는지 확인하고
-										if(union(curTmp[i][j], curTmp[nx][ny], parentsTmp)) {
-											//연결이 안되어있으면 연결하고 해당 상태로 다음 분기 진행
-											DFS(curTmp, parentsTmp);
-										}
-										//while문 나가기
-										break;
+								//밖으로 나가면 큐에 추가하지 않고 break
+								if(!isIn(nx,ny)) break;
+								//다음좌표가 같은 섬이면 탐색 종료
+								if(map[nx][ny] == map[i][j]) break;
+								//섬을 만나면 같은 섬인지 확인하고 아니면 큐에 추가
+								if(map[nx][ny] != 0 && map[i][j]!= map[nx][ny]) {
+									//다리길이 2이상인지 체크
+									if(len - 1 > 1) {
+										//pq에 해경로 추가
+										pq.offer(new Path(map[i][j], map[nx][ny], len-1));
 									}
-								} else {
-									//내부가 아니면 while문 나가기
 									break;
 								}
 							}
 						}
-					}
+ 					}
 				}
 			}
 		}
+	}
+	
+	//union-find 관련
+	//부모배열 생성 
+	private static void make() {
+		parents = new int[areaNum];
+		for (int i = 0; i < areaNum; i++) {
+			parents[i] = i;
+		}
+	}
+	
+	//find
+	//root 찾기
+	private static int find(int a) {
+		if(parents[a] == a) return a;
+		return parents[a] = find(parents[a]);
+	}
+	
+	//union
+	//루트 비교 후 합치기
+	private static boolean union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		if(aRoot == bRoot) return false;
+		parents[aRoot] = bRoot;
+		return true;
+	}
+	
+	//모든 섬의 root가 같은지 확인
+	private static boolean endCheck() {
+		int root = find(parents[1]);
+		for (int i = 2; i < areaNum; i++) {
+			if(find(i) != root) return false;
+		}
+		return true;
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -155,37 +144,41 @@ public class Main_17472_김민석 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
+		int answer = 0;
 		map = new int[N][M];
-		visited = new boolean[N][M];
-		answer = Integer.MAX_VALUE;
-		
+		//섬 시작 번호
+		areaNum = 1;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+				//섬 번호를 1번부터 시작하기 위해 1 -> -2로 넣기
+				map[i][j] = Integer.parseInt(st.nextToken()) * -2;
 			}
 		}
 		
-		//지역번호세팅
+		//섬 번호 세팅
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if(map[i][j] == 1 && !visited[i][j]) {
-					setArea(i,j);
-				}
+				if(map[i][j] == -2) setArea(i,j);
 			}
 		}
 		
-		//지역번호1부터 시작하는 부모 배열 만들기
-		int[] parents = new int[areaNum];
+		//될 수 있는 다리 다 구하기
+		setRoutes();
 		
-		//union-find make작업
-		for (int i = 0; i < parents.length; i++) {
-			parents[i] = i;
+		//union-find root배열 생성
+		make();
+		while(!pq.isEmpty()) {
+			//pq에서 가장 작은 길이의 다리 가져옴
+			Path cur = pq.poll();
+			//합칠 수 있다면 합치고 최종 길이에 추가
+			if(union(cur.start, cur.end)) {
+				answer += cur.len;
+			}
+			//모든 섬 연결 체크
+			if(endCheck()) break;
 		}
-		
-		//탐색시작
-		DFS(map, parents);
-		//정답이 갱신되지 않았다면 불가능하기 때문에 -1출력 
-		System.out.println(answer==Integer.MAX_VALUE ? -1:answer);
+		if(!endCheck()) answer = 0;
+		System.out.println(answer == 0 ? -1:answer);
 	}
 }
